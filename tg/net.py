4e3e71df5ded78fe46aab1edc0be348fda00d82c
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2025-04-27 12:32:00 +0300

    remove base_url

--- tg/net.py
+++ tg/net.py
@@ -1,4 +1,4 @@
 import httpx
 
 timeout = httpx.Timeout(timeout=5, connect=2, read=120)
-client = httpx.AsyncClient(base_url="https://api.telegram.org/bot", timeout=timeout)
+client = httpx.AsyncClient(timeout=timeout)
